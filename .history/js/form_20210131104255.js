9d68d21fdf42561c8b234852a4a9aee3774724de
dubar jeremy <jeremy.dubar@epitech.eu>
2021-02-06 00:37:46 +0100

    Add desktop view

---
+++ .history/js/form_20210131104255.js
@@ -0,0 +1,37 @@
+window.onload = function() {
+
+  const plus = document.querySelector(".plus").addEventListener("click", increase);
+  const minus = document.querySelector(".minus").addEventListener("click", decrease);
+
+  let i = 0;
+  let initialPrice = 50;
+  let priceAfterDiscount = increase();
+
+  function increase(e) {
+      i++
+        console.log(e.target);
+      let items = document.querySelector(".nbr-value").value = i;
+      let qty = parseInt(items);
+      let newPrice = initialPrice * qty;
+      let priceBeforeDiscount = newPrice; // impossible de renommer newPrice ligne15. Obligé de re assigner ici. Pourquoi ?
+      document.querySelector(".old-price").innerHTML = priceBeforeDiscount;
+      newPrice -= newPrice * 10 / 100;
+      let priceAfterDiscount = newPrice; // meme chose ici pour ligne 18
+      document.querySelector(".new-price").innerHTML = priceAfterDiscount;
+      return priceAfterDiscount;
+  }
+
+  function decrease() {
+      if (i > 1) {
+          i--
+          let items = document.querySelector(".nbr-value").value = i;
+          let qty = parseInt(items);
+          let newPrice = initialPrice * qty;
+          document.querySelector(".new-price").innerHTML = newPrice;
+          let oldPrice = qty * priceAfterDiscount;
+          document.querySelector(".old-price").innerHTML = oldPrice;
+          console.log(oldPrice);
+      }
+  }
+
+}
